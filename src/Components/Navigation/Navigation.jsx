ab92b01eab93a37063bd9e2376dfd3a4db8e7956
Jun <jz7259@g.rit.edu>
2024-09-29 22:00:57 -0400

    Scroll and click closes navigation

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navigation.module.css";
 
 export const Navigation = () => {
+  const checkboxRef = useRef(null);
+  const navRef = useRef(null);
+
+  const handleScroll = () => {
+    // Automatically close the navbar on scroll
+    if (checkboxRef.current) {
+      checkboxRef.current.checked = false;
+    }
+  };
+
+  const handleClickOutside = (event) => {
+    // If click is outside of the navigation, uncheck the checkbox
+    if (navRef.current && !navRef.current.contains(event.target)) {
+      if (checkboxRef.current) {
+        checkboxRef.current.checked = false;
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Add scroll event listener
+    window.addEventListener("scroll", handleScroll);
+    // Add click event listener for detecting clicks outside the navbar
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      // Clean up the event listeners
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <nav>
+    <nav ref={navRef}>
       <div>
-        <a className={styles.nav__home} href ="/">FJ</a>
+        <a className={styles.nav__home} href="/">
+          FJ
+        </a>
       </div>
       <div className={styles.nav__ham}>
-        <input type="checkbox" className={styles.nav__checkbox} id="checkbox_toggle" />
+        <input
+          type="checkbox"
+          className={styles.nav__checkbox}
+          id="checkbox_toggle"
+          ref={checkboxRef}
+        />
         <label htmlFor="checkbox_toggle" className={styles.nav__hamburgerButton}>
           <ul>
             <li className={styles.hamburgerButton__topStroke}></li>
@@ -20,7 +59,7 @@ export const Navigation = () => {
         <div className={styles.nav__content}>
           <ul>
             <li>
-              <Link to="/photo-gallery">View Gallery</Link> {/* Updated Link */}
+              <Link to="/photo-gallery">View Gallery</Link>
             </li>
             <li>
               <a href="#">Log In</a>
